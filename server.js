6869d2ed2a3d8e60d7577a34be23a8ddb3530ed4
prakshh <prakashdas18@gmail.com>
2025-03-08 20:33:19 +0530

    added class for all boards

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const boardRoutes = require("./routes/boardRoutes"); // Import board routes
+const classRoutes = require("./routes/classRoutes"); // Add this
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,7 @@ app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/boards", boardRoutes); // Register board routes
+app.use("/api/classes", classRoutes); // Add this
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
